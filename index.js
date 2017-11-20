cacbe1f876ff641f9367910b87d9b98fb0001168
Linus <linus@ronaldjacob.de>
2017-11-20 20:08:54 +0100

    Added functionality to next + prev buttons

--- index.js
+++ index.js
@@ -5,10 +5,55 @@ addEvent (document, "DOMContentLoaded", function () {
     arrows_right = document.getElementsByClassName("arrowbox-right");
     
     function nextImg (carousel) {
-        let active = document.querySelector(`#${carousel} > .active`);
-        let id = active.id;
-        console.log(id);
+        
+        if (carousel === "carousel1") {
+            var imageClassName = "teamproject_img";
+        } else {
+            var imageClassName = "singleproject_img";
+        }
+
+        let active = document.querySelector(`#${carousel} > .active`),
+        images = document.getElementsByClassName(imageClassName);
+        items = images.length, 
+        id = active.id.split("-");
+        
+        if (!Number(id[1])) { console.log("Error: Invalid class-name") }
+        else if(id[1] == items) { id[1] = "1"}
+        else {
+            id[1] = (Number(id[1]) + 1).toString();
+        }
+
+        newID = id.join("-");
+        active.className = imageClassName;
+        document.getElementById(newID).className = `${imageClassName} active`;
+    }
+    
+    function prevImg (carousel) {
+        
+        if (carousel === "carousel1") {
+            var imageClassName = "teamproject_img";
+        } else {
+            var imageClassName = "singleproject_img";
+        }
+
+        let active = document.querySelector(`#${carousel} > .active`),
+        images = document.getElementsByClassName(imageClassName);
+        items = images.length, 
+        id = active.id.split("-");
+        
+        if (!Number(id[1])) { console.log("Error: Invalid class-name") }
+        else if(id[1] == 1) { id[1] = items.toString();}
+        else {
+            id[1] = (Number(id[1]) - 1).toString();
+        }
+
+        newID = id.join("-");
+        active.className = imageClassName;
+        document.getElementById(newID).className = `${imageClassName} active`;
     }
     
     addEvent(arrows_right[0], "click", function () {nextImg("carousel1")});
+    addEvent(arrows_right[1], "click", function () {nextImg("carousel2")});
+    addEvent(arrows_left[0], "click", function () {prevImg("carousel1")});
+    addEvent(arrows_left[1], "click", function () {prevImg("carousel2")});
 });
