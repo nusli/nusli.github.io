41854766fbca42082e2a0f304f4eebe4b329b93b
Linus <linus@ronaldjacob.de>
2017-12-12 18:27:27 +0100

    Added layers to portfolio

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 addEvent (document, "DOMContentLoaded", function () {
     console.log("DOM content loaded");
     
+    // functions to change portfolio images
     const arrows_left = document.getElementsByClassName("arrowbox-left"),
     arrows_right = document.getElementsByClassName("arrowbox-right");
     
@@ -26,6 +27,7 @@ addEvent (document, "DOMContentLoaded", function () {
         newID = id.join("-");
         active.className = imageClassName;
         document.getElementById(newID).className = `${imageClassName} active`;
+        hideLayer(active);
     }
     
     function prevImg (carousel) {
@@ -42,6 +44,7 @@ addEvent (document, "DOMContentLoaded", function () {
         id = active.id.split("-");
         
         if (!Number(id[1])) { console.log("Error: Invalid class-name") }
+        /* Select previous Image */
         else if(id[1] == 1) { id[1] = items.toString();}
         else {
             id[1] = (Number(id[1]) - 1).toString();
@@ -49,11 +52,38 @@ addEvent (document, "DOMContentLoaded", function () {
 
         newID = id.join("-");
         active.className = imageClassName;
-        document.getElementById(newID).className = `${imageClassName} active`;
+        document.getElementById(newID).className = `${imageClassName} active`; //Assign 'active'class
+        hideLayer(active);
     }
     
     addEvent(arrows_right[0], "click", function () {nextImg("carousel1")});
     addEvent(arrows_right[1], "click", function () {nextImg("carousel2")});
     addEvent(arrows_left[0], "click", function () {prevImg("carousel1")});
     addEvent(arrows_left[1], "click", function () {prevImg("carousel2")});
+
+    /* functions to show image layer in portfolio */
+    const teamProjects = document.getElementsByClassName("teamproject_img"),
+    singleProjects =  document.getElementsByClassName("singleproject_img");
+
+    function showLayer () {
+        let anchor = this.children[0];
+        anchor.style.height = "96%"
+        anchor.style.padding = "4% 4% 0 4%";
+    }
+    function hideLayer ( element ) {
+        let layer = element.children[0];
+        if (layer.style.height !== 0) {
+            layer.style.height = 0;
+            layer.style.padding = "0 4% 0 4%";
+        }
+    }
+    //addEvent(teamProjects[1], "click", showLayer);
+
+    for (let i=0; i < teamProjects.length; i++) {
+        addEvent(teamProjects[i], "click", showLayer);
+    } 
+    
+    for (let i=0; i < singleProjects.length; i++) {
+        addEvent(singleProjects[i], "click", showLayer);
+    }
 });
